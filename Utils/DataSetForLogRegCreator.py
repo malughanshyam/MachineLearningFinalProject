0aae35093e81f29310f7e6f53cff7878b9e8a601
niharkhetan <nihar.khetan@gmail.com>
2015-11-29 22:54:50 -0500

    Dataset prepared for our Logitic Regression code Dataset 7 and 8 added Normalized versions of 7 and 8 also added
    Change-Id: I151398d7a8a6467aece5199c6d4f3fc022c8641c
    Signed-off-by: niharkhetan <nihar.khetan@gmail.com>

--- Utils/DataSetForLogRegCreator.py
+++ Utils/DataSetForLogRegCreator.py
@@ -6,20 +6,28 @@ Created on Nov 29, 2015
 from sklearn import preprocessing
 from DataParser.CSVHandler import *
 
+
+def dataStringDiscreteToNumericEncoder(vector):
+    colNumToEncode = [0,2,3,4,6,7,8,17]
+    
+    le = preprocessing.LabelEncoder()
+    for colNum in colNumToEncode:
+        colToEncode = []
+        for i in range(1, len(vector)):
+            colToEncode.append(vector[i][colNum])
+        le.fit(colToEncode)
+        encodedColumn = le.transform(colToEncode)       
+        for i in range(1, len(vector)):
+            vector[i][colNum] = encodedColumn[i - 1]
+    return vector
+
 if __name__ == '__main__':
     
-    trainingData = "training/training_dataset_4_unbalanced_normalized.csv"
+    datasetDir = os.path.join(os.pardir,'Dataset','main')
+    balancedDataFile = os.path.join(datasetDir,'dataset_7_balanced.csv')
+    trainingData = "main/dataset_3_balanced_normalized.csv"
     vector = readFileAsVector(trainingData)
-    vec = ["paris", "paris", "paris", "tokyo","amsterdam"]
-    le = preprocessing.LabelEncoder()
-    le.fit(["paris", "paris", "tokyo", "amsterdam"])
-    print list(le.classes_)
-    vec = le.transform(vec)
-    print vec
-    print vector[0]
-    print vector[1]
-
-    for i in range(len(vector[0])):
-        print vector[0][i], "(",i,") ->", vector[1][i]
-    colToEncode = [0,2,3,4,6,7,8,17]
+    encodedVector = dataStringDiscreteToNumericEncoder(vector)   
+    writeCSVFile(balancedDataFile, encodedVector)
+    print "Done"
     
