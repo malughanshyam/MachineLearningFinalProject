1e39730854be1fdcc105a77168a5177387388a89
Ghanshyam Malu <malughanshyam@github.com>
2015-12-01 04:45:01 -0500

    Updated charts in slides

--- Utils/TrainingTestDataExtractor.py
+++ Utils/TrainingTestDataExtractor.py
@@ -7,7 +7,9 @@ import random, pickle
 from DataParser.CSVHandler import *
 
 def getTrainingAndTestIndices(trainingDataPercent, totalDataSize):
+     
     ''' Training and Test Row numbers based on the size of dataset '''
+
     trainingWgt = int(trainingDataPercent * totalDataSize)
     
     allIndices = range(1,totalDataSize)
@@ -16,6 +18,35 @@ def getTrainingAndTestIndices(trainingDataPercent, totalDataSize):
     
     return trainingRowNum, testRowNum
     
+def createNewTrainingAndTestIndices():
+    
+    #      ['dataset_0_balanced.csv', 'dataset_0_unbalanced.csv', 'dataset_1_balanced_discretized.csv', 'dataset_2_unbalanced_discretized.csv', 'dataset_3_balanced_normalized.csv', 'dataset_4_unbalanced_normalized.csv', 'dataset_5_balanced_handpicked_features.csv', 'dataset_6_unbalanced_handpicked_features.csv']]
+    vector = readFileAsVector('main/dataset_0_balanced.csv')
+    trainingRowNumBalanced, testRowNumBalanced = getTrainingAndTestIndices(trainingDataPercent, len(vector))
+    
+    vector = readFileAsVector('main/dataset_0_unbalanced.csv')
+    trainingRowNumUnBalanced, testRowNumUnBalanced = getTrainingAndTestIndices(trainingDataPercent, len(vector))
+    
+    
+    allIndexDict = {
+                 'trainingRowNumBalanced':trainingRowNumBalanced,
+                 'testRowNumBalanced':testRowNumBalanced,
+                 'trainingRowNumUnBalanced':trainingRowNumUnBalanced,
+                 'testRowNumUnBalanced':testRowNumUnBalanced
+                 }
+    output = open(os.path.join(datasetDir,'TrainingAndTestIndexDictPickle.pkl'), 'wb')
+    pickle.dump(allIndexDict, output)
+    output.close()
+    
+    return allIndexDict
+
+def getExistingTrainingAndTestIndices():
+    pkl_file = open(os.path.join(datasetDir,'TrainingAndTestIndexDictPickle.pkl'), 'rb')
+    allIndexDict = pickle.load(pkl_file)
+
+    pkl_file.close()
+    return allIndexDict
+
 def extractRows(datasetVector,trainingRowNumBalanced):
     
     extractedRows = [datasetVector[0]]     
@@ -31,27 +62,10 @@ if __name__ == '__main__':
     trainDataDir = os.path.join(os.pardir,'Dataset','train')
     testDataDir = os.path.join(os.pardir,'Dataset','test')
     
-#     ['dataset_0_balanced.csv', 'dataset_0_unbalanced.csv', 'dataset_1_balanced_discretized.csv', 'dataset_2_unbalanced_discretized.csv', 'dataset_3_balanced_normalized.csv', 'dataset_4_unbalanced_normalized.csv', 'dataset_5_balanced_handpicked_features.csv', 'dataset_6_unbalanced_handpicked_features.csv']]
-    vector = readFileAsVector('main/dataset_0_balanced.csv')
-    trainingRowNumBalanced, testRowNumBalanced = getTrainingAndTestIndices(trainingDataPercent, len(vector))
-    
-    vector = readFileAsVector('main/dataset_0_unbalanced.csv')
-    trainingRowNumUnBalanced, testRowNumUnBalanced = getTrainingAndTestIndices(trainingDataPercent, len(vector))
-
-    
-    allIndexDict = {
-                 'trainingRowNumBalanced':trainingRowNumBalanced,
-                 'testRowNumBalanced':testRowNumBalanced,
-                 'trainingRowNumUnBalanced':trainingRowNumUnBalanced,
-                 'testRowNumUnBalanced':testRowNumUnBalanced
-                 }
-    output = open(os.path.join(datasetDir,'TrainingAndTestIndexDictPickle.pkl'), 'wb')
-    pickle.dump(allIndexDict, output)
-    output.close()
+#     allIndexDict = createNewTrainingAndTestIndices()
+    allIndexDict = getExistingTrainingAndTestIndices()
     
     allDatasetNameList = os.listdir(datasetDir)
-    allDatasetNameList.remove('dataset_0_balanced.csv')
-    allDatasetNameList.remove('dataset_0_unbalanced.csv')
     allDatasetNameList.remove('raw')
     allDatasetNameList.remove('training.csv')
     allDatasetNameList.remove('TrainingAndTestIndexDictPickle.pkl')
@@ -70,12 +84,12 @@ if __name__ == '__main__':
         datasetVector = readFileAsVector(os.path.join(datasetDir,dataset))
         print
         print "Extracting", str(trainingDataPercent*100) +'%', "Train dataset for", dataset
-        trainData = extractRows(datasetVector,trainingRowNumBalanced)
+        trainData = extractRows(datasetVector,allIndexDict['trainingRowNumBalanced'])
         trainDataFileName = os.path.join(trainDataDir,'train_'+ dataset)
         writeCSVFile(trainDataFileName, trainData)
 
         print "Extracting ", str((1-trainingDataPercent)*100) +'%', "Test dataset for", dataset
-        testData = extractRows(datasetVector,testRowNumBalanced)
+        testData = extractRows(datasetVector,allIndexDict['testRowNumBalanced'])
         testDataFileName = os.path.join(testDataDir,'test_'+ dataset)
         writeCSVFile(testDataFileName, testData)        
         
@@ -87,12 +101,12 @@ if __name__ == '__main__':
         datasetVector = readFileAsVector(os.path.join(datasetDir,dataset))
         print
         print "Extracting", str(trainingDataPercent*100) +'%', "Train dataset for", dataset
-        trainData = extractRows(datasetVector,trainingRowNumUnBalanced)
+        trainData = extractRows(datasetVector,allIndexDict['trainingRowNumUnBalanced'])
         trainDataFileName = os.path.join(trainDataDir,'train_'+ dataset)
         writeCSVFile(trainDataFileName, trainData)
 
         print "Extracting ", str((1-trainingDataPercent)*100) +'%', "Test dataset for", dataset
-        testData = extractRows(datasetVector,testRowNumUnBalanced)
+        testData = extractRows(datasetVector,allIndexDict['testRowNumUnBalanced'])
         testDataFileName = os.path.join(testDataDir,'test_'+ dataset)
         writeCSVFile(testDataFileName, testData)        
         
